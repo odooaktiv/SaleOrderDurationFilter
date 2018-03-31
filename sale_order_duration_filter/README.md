55843a004bb14f7fa7a22491f201d3acff478949
odoo <alpesh@aktivsoftware.com>
2018-03-31 16:27:40 +0530

    [ADD] initial commit in master.

---
+++ sale_order_duration_filter/README.md
@@ -0,0 +1,21 @@
+
+------------------------------------
+
+Odoo Version : Odoo 11.0 Community
+
+
+Installation 
+-------------------------------------
+Install the Application => Apps -> Sale Order Duration Filter(Technical Name: sale_order_duration_filter)
+
+
+Overview
+-------------------------------------
+-> Filters added here are:
+* Today 
+* Yesterday
+* Last Week
+* Last Month
+* Last Year
+
+-> This module helps to get the filtered records based on the above filters in sales orders as well as order quotations.
---
+++ sale_order_duration_filter/__manifest__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Sale Order Duration Filter',
+    'category': 'sale',
+    'version': '1.1',
+    'summary': 'This module manages the filtering based on duration of sales orders as well as quotations',
+    'website': 'http://www.aktivsoftware.com',
+    'author': 'Aktiv Software',
+    'license': 'AGPL-3',
+    'description': 'Manages duration filtering for sale orders and quotations.',
+    'depends': ['base', 'sale'],
+    'data': [
+        'views/sale_order_filter_views.xml',
+    ],
+    'images': [],
+    'images': ['static/description/banner.jpg'],
+    'auto_install': False,
+    'installable': True,
+    'application': False,
+}
