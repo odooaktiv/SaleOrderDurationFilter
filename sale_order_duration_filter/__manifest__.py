bd392b7bba83353904f4a8db7c7c253e3354d9d3
Odoo Aktiv <solankiriddhir@yahoo.in>
2018-06-30 11:16:13 +0530

    [IMP] pep8 standards for branch 10.0

--- sale_order_duration_filter/__manifest__.py
+++ sale_order_duration_filter/__manifest__.py
@@ -3,11 +3,13 @@
     'name': 'Sale Order Duration Filter',
     'category': 'sale',
     'version': '1.1',
-    'summary': 'This module manages the filtering based on duration of sales orders as well as quotations',
+    'summary': 'This module manages the filtering based on duration of sales\
+     orders as well as quotations',
     'website': 'http://www.aktivsoftware.com',
     'author': 'Aktiv Software',
     'license': 'AGPL-3',
-    'description': 'Manages duration filtering for sale orders and quotations.',
+    'description': 'Manages duration filtering for sale orders\
+     and quotations.',
     'depends': ['base', 'sale'],
     'data': [
         'views/sale_order_filter_views.xml',
